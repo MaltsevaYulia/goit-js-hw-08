a19ae9ef661104e0f2a9f57d2a5bde1d794dcd96
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-01-20 11:05:46 +0200

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
+import simpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galaryContainer=document.querySelector('.gallery')
+const galaryMarkup = createGalaryMarkup(galleryItems);
+galaryContainer.insertAdjacentHTML('beforeend',galaryMarkup)
+
+function createGalaryMarkup(galleryItems) {
+    return galleryItems.map(({preview,original,description}) =>
+        `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    ).join('')
+
+}
+
+
+const lightbox = new SimpleLightbox('.gallery a', { /* options */captionsData: 'alt',captionPosition:'bottom',captionDelay:	250 });
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
 
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state"
+const formData={}
+
+const refs={
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('input', throttle(onInputChange,500));
+refs.form.addEventListener('submit', onSubmit)
+onPagaloading()
+function onInputChange(evt) {
+    formData[evt.target.name]=evt.target.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
+}
+
+function onPagaloading() {
+    const localStorageData = localStorage.getItem(STORAGE_KEY);
+    // console.log("🚀 localStorageDat", localStorageData)
+
+    if(localStorageData){
+        
+        refs.input.value=JSON.parse(localStorageData).email
+        refs.textarea.value=JSON.parse(localStorageData).message
+
+    }
+}
+
+function onSubmit(evt) {
+    const localStorageData = localStorage.getItem(STORAGE_KEY);
+    const localStorageObj=JSON.parse(localStorageData);
+    evt.preventDefault();
+    evt.target.reset() ;
+    console.log(localStorageObj);
+    localStorage.removeItem(STORAGE_KEY);
+    
+
+}
